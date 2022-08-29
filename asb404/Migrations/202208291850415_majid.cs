7236408a69531536aaa933ab0bbbe2e8c41c840a
p30max <amax2008@gmail.com>
2022-08-29 23:41:48 +0430

    لال

---
+++ asb404/Migrations/202208291850415_majid.cs
@@ -0,0 +1,19 @@
+﻿namespace Asb404.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class majid : DbMigration
+    {
+        public override void Up()
+        {
+
+            
+        }
+        
+        public override void Down()
+        {
+ 
+        }
+    }
+}
